3b46a0ed422bde6d91e891195d806573e373e743
bnsoni <bhargavsoni8@gmail.com>
2021-10-17 23:24:36 +0300

    An xBlock on OpenEdx

---
+++ bnsblock/__init__.py
@@ -0,0 +1,1 @@
+from .bnsblock import BnsXBlock
---
+++ bnsblock/static/js/src/bnsblock.js
@@ -0,0 +1,70 @@
+/* Javascript for BnsXBlock. */
+function BnsXBlock(runtime, element) {
+
+    function updateCount(result) {
+        $('.count', element).text(result.count);
+    }
+
+    function updateApiData(return_data){
+        $('.apidata', element).text(return_data);
+    }
+
+    var handlerUrl = runtime.handlerUrl(element, 'increment_count');
+
+    
+
+    $('#get_num').click(function(eventObject) {
+
+        $.get("http://localhost:8000/api/interview/", function(data, status){
+            alert("Magic Number: " + /*JSON.stringify(data)*/ data[0].greeting + "\nStatus: " + status);
+        });
+
+
+    });
+
+
+    $('#post_num').click(function(eventObject) {
+        var mObject = new Object();
+        mObject.id = 1;
+        mObject.greeting = document.getElementById('magic_num').value;
+
+        //alert(JSON.stringify(mObject));
+
+        $.ajax({
+            type: "PUT",
+            url: "http://localhost:8000/api/interview/update/1/",
+            data: mObject,
+            success: function(data, status){
+                alert(data + "\nStatus: " + status);
+            }
+        });
+
+
+    });
+
+    $(function ($) {
+        /* Here's where you'd do things on page load. */
+    });
+}
+
+
+
+
+        //$.ajax({
+        //    type: "POST",
+        //    url: handlerUrl,
+        //    data: JSON.stringify({"hello": "world"}),
+        //    success: updateCount
+        //});
+        
+        //$.ajax({
+//            type: "GET",
+//            url: "http://localhost:8000/api/interview/",
+  //          data:{
+    //           //data to be sent to the server.
+      //      },
+        //    success: function(response){
+          //      //$('.apidata', element).text(response);
+            //    $('.apidata', element).text(response);
+            //}
+        //});
